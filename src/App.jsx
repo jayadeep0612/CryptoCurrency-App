32acd117796abff46dd57bed0e12600562ef85e6
Jayadeep <kajayadeep@gmail.com>
2025-08-12 23:54:27 +0530

    main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
-
+import { Routes, Route, Link } from 'react-router-dom';
+import {Layout,Typography,Space} from 'antd'
+import  NavBar  from './components/navbar';
 import './App.css'
 
+
 function App() {
-  <>
-  </>
-  
+  return(
+  <div className='app'>
+    <div className='navbar'>
+      <NavBar/>
+    </div>
+    <div className='main'>
+
+    </div>
+    <div className='footer'>
+
+    </div>
+  </div>
+  )
 }
 
 export default App
